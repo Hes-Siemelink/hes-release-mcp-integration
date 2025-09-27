6821851dc3b4f69d3f5a64b3cd78087b8141c8c7
Hes Siemelink <hsiemelink@xebialabs.com>
2025-09-27 06:28:02 +0200

    Initial commit

---
+++ src/sample_release_api_task.py
@@ -0,0 +1,36 @@
+from digitalai.release.integration import BaseTask
+
+
+class SetSystemMessage(BaseTask):
+    """
+        Sets the system message using the Release API client.
+
+        Preconditions:
+            - The 'Run as user' property must be set on the release.
+            - The executing user must have valid credentials.
+    """
+
+    def execute(self) -> None:
+
+        # Get the message from the input properties
+        message = self.input_properties['message']
+        if not message:
+            raise ValueError("The 'Message' field cannot be empty")
+
+        # Obtain an instance of the Release API client
+        release_api_client = self.get_release_api_client()
+
+        # Prepare the payload for the API request
+        system_message = {
+            "type": "xlrelease.SystemMessageSettings",
+            "id" : "Configuration/settings/SystemMessageSettings",
+            "message": message,
+            "enabled": "True",
+            "automated": "False"
+        }
+
+        # Send a PUT request to update the system message configuration
+        release_api_client.put("/api/v1/config/system-message", json=system_message)
+
+        # Add a line to the comment section in the UI
+        self.add_comment(f"System message updated to \"{message}\"")

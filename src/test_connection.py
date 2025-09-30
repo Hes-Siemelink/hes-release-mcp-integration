a8482d7368350e7614f38cae45f827094cde10b5
Hes Siemelink <hsiemelink@digital.ai>
2025-09-30 21:13:50 +0200

    Adds test connection and tool lookup script

---
+++ src/test_connection.py
@@ -0,0 +1,38 @@
+import asyncio
+
+from digitalai.release.integration import BaseTask
+from fastmcp import Client
+
+from src.call_tool_task import create_transport
+
+
+class TestConnection(BaseTask):
+    """
+        Testing connection to the remote server
+    """
+
+    def execute(self) -> None:
+
+        try:
+            # Process input
+            server = self.input_properties['server']
+            if server is None:
+                raise ValueError("Server field cannot be empty")
+
+            # Make request
+            transport = create_transport(server)
+            client = Client(transport)
+            output = asyncio.run(ping(client))
+
+            # Process result
+            result = {"success": True, "output": "Connection success"}
+        except Exception as e:
+            result = {"success": False, "output": str(e)}
+        finally:
+            self.set_output_property("commandResponse", result)
+
+
+# Async method to call tool
+async def ping(client):
+    async with client:
+        return await client.ping()
---
+++ src/tool_lookup_script.py
@@ -0,0 +1,38 @@
+import asyncio
+
+from digitalai.release.integration import BaseTask
+from fastmcp import Client
+
+from src.call_tool_task import create_transport
+from src.list_tools_task import list_tools
+
+
+class ToolLookupScript(BaseTask):
+    """
+        Testing connection to the remote server
+    """
+
+    def execute(self) -> None:
+
+        try:
+            # Process input
+            server = self.input_properties["_ci"]['server']
+            if server is None:
+                raise ValueError("Server field cannot be empty")
+
+            # Make request
+            transport = create_transport(server)
+            client = Client(transport)
+            output = asyncio.run(list_tools(client))
+
+            # Process result
+            result = to_tool_result(output)
+        except Exception as e:
+            result = []
+
+        self.set_output_property("commandResponse", result)
+
+
+def to_tool_result(output):
+    # Build a list of dicts with 'label' and 'value' keys
+    return [{'label': tool.name, 'value': tool.name} for tool in output]

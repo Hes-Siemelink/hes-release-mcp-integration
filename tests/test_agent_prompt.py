cd5b1fd1d511958303f4ceff9199b961666a253a
Hes Siemelink <hsiemelink@digital.ai>
2025-10-04 08:27:13 +0200

    Adds example with Release MCP

--- tests/test_agent_prompt.py
+++ tests/test_agent_prompt.py
@@ -29,3 +29,30 @@ class TestAgentPrompt(unittest.TestCase):
 
         # Then
         self.assertIn('Hola', result)
+
+    def test_with_release_mcp(self):
+        # Given
+        load_dotenv()
+        api_key = os.getenv('GEMINI_API_KEY')
+
+        task = AgentPrompt()
+        task.input_properties = {
+            'prompt': 'What is the latest failed release',
+            'model': {
+                'apiKey': api_key,
+                'model': 'gemini-2.5-flash'
+            },
+            'mcpServer1': {
+                'title': 'Release MCP',
+                'url': 'http://localhost:8000/mcp',
+                'transport': 'http',
+            }
+        }
+
+        # When
+        task.execute_task()
+        result = task.get_output_properties()['result']
+        print(result)
+
+        # Then
+        # self.assertIn('Hola', result)
